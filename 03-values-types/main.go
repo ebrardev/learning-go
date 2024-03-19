3a69d0101c624698d1a7709a42e76e7c88f8d53d
ebrardev <16155052@mersin.edu.tr>
2024-03-19 11:18:25 +0300

    Refactor variable declarations and add user input prompts

--- 03-values-types/main.go
+++ 03-values-types/main.go
@@ -8,10 +8,17 @@ import (
 func main() {
 
 	const inflationRate = 2.5
-	var investmentAmount float64
-	expectedReturn := 0.1
-	years := 10.0
+	// var investmentAmount float64
+	// var expectedReturn float64
+	// var years float64
+	var investmentAmount, expectedReturn, years float64
+
+	fmt.Println("Enter the investment amount: ")
 	fmt.Scan(&investmentAmount)
+	fmt.Println("Enter the expected return: ")
+	fmt.Scan(&expectedReturn)
+	fmt.Println("Enter the years: ")
+	fmt.Scan(&years)
 	futureValue := investmentAmount * math.Pow(1+expectedReturn/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
