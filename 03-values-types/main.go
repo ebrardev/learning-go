f91c6a56ee808d860ed4a9080ed2eb5fc463734d
ebrardev <16155052@mersin.edu.tr>
2024-03-19 09:38:15 +0300

    Refactor variable declarations in main.go

--- 03-values-types/main.go
+++ 03-values-types/main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 
-	var investmentAmount float64 = 1000
-	var expectedReturn = 0.1
-	var years float64 = 10
+	var investmentAmount, years float64 = 1000, 10
+	expectedReturn := 0.1
+
 	var futureValue = investmentAmount * math.Pow(1+expectedReturn/100, years)
 	fmt.Println(futureValue)
 
