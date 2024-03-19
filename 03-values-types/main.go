45c50577ec8a8ce14ab31764ecf2e316ed65e71f
ebrardev <16155052@mersin.edu.tr>
2024-03-19 09:46:27 +0300

    Add future real value calculation

--- 03-values-types/main.go
+++ 03-values-types/main.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 
+	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	expectedReturn := 0.1
 	years := 10.0
-	var futureValue = investmentAmount * math.Pow(1+expectedReturn/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturn/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
