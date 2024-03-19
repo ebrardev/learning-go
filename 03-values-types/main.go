faa1c0db233ebef727625117dbd8da3e56c3e1d8
ebrardev <16155052@mersin.edu.tr>
2024-03-19 09:40:33 +0300

    Refactor investment calculation in main.go

--- 03-values-types/main.go
+++ 03-values-types/main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 
-	var investmentAmount, years float64 = 1000, 10
+	var investmentAmount float64 = 1000
 	expectedReturn := 0.1
-
+	years := 10.0
 	var futureValue = investmentAmount * math.Pow(1+expectedReturn/100, years)
 	fmt.Println(futureValue)
 
