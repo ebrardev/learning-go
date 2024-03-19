750147ab16441ac66de2d47da1d0773a24f926a6
ebrardev <16155052@mersin.edu.tr>
2024-03-19 16:45:40 +0300

    Fix bug in login functionality

---
+++ 05-formatting/go.mod
@@ -0,0 +1,3 @@
+module github.com/formatting
+
+go 1.22.0
---
+++ 05-formatting/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var revenue, expenses, taxtRate float64
+
+	fmt.Println("Enter the revenue: ")
+	fmt.Scan(&revenue)
+	fmt.Println("Enter the expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Println("Enter the tax rate: ")
+	fmt.Scan(&taxtRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxtRate/100)
+	ratio := profit / revenue
+	fmt.Println("ebt %v\nebtt :%v", ebt)
+	fmt.Println("profit", profit)
+	fmt.Println(ratio)
+
+}
