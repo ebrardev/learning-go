586396bf7be06e51e790b74bda2fa9c87476ea8e
ebrardev <16155052@mersin.edu.tr>
2024-03-25 01:02:43 +0300

    Add menu options for bank operations

--- 07-if statetements/bank.go
+++ 07-if statetements/bank.go
@@ -7,5 +7,24 @@ import (
 func main() {
 
 	fmt.Println("Welcome to the Bank of Golang")
+	fmt.Println("What would you like to do today?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit")
+	fmt.Println("3. Withdraw")
+	fmt.Println("4. Exit")
 
+	var choice int
+	fmt.Print("Enter your choice: ")
+	fmt.Scan(&choice)
+
+
+
+	wantsCheckBalance:= choice == 1
+     
+	  if wantsCheckBalance {
+
+	  }
+	fmt.Println("Your choice is: ", choice)
+ 
+	
 }
---
+++ 07-if statetements/go.mod
@@ -0,0 +1,3 @@
+module github.com/ebrargo
+
+go 1.22.0
