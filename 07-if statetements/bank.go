1f97832d16b2ad6e02e9696ea866936bcbf2ce72
ebrardev <16155052@mersin.edu.tr>
2024-03-25 01:52:51 +0300

    Refactor bank.go to handle invalid deposit amount and insufficient balance

--- 07-if statetements/bank.go
+++ 07-if statetements/bank.go
@@ -6,47 +6,55 @@ import (
 
 func main() {
 	var accountBalance float64 = 1000.0
-	var depositAmount float64 = 0.0
-	var WithdrawAmount float64
-	fmt.Println("Welcome to the Bank of Golang")
-	fmt.Println("What would you like to do today?")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit")
-	fmt.Println("3. Withdraw")
-	fmt.Println("4. Exit")
+	var depositAmount, WithdrawAmount float64
 
-	var choice int
-	fmt.Print("Enter your choice: ")
-	fmt.Scan(&choice)
+	for i := 0; i < 3; i++ {
+		fmt.Println("Welcome to the Bank of Golang")
+		fmt.Println("What would you like to do today?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit")
+		fmt.Println("3. Withdraw")
+		fmt.Println("4. Exit")
 
-	// wantsCheckBalance:= choice == 1
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scan(&choice)
 
-	if choice == 1 {
-		fmt.Println("Checking your balance")
-		fmt.Println("Your account balance is: ", accountBalance)
+		// wantsCheckBalance:= choice == 1
 
-	} else if choice == 2 {
-		fmt.Println("Deposit")
-		fmt.Print("Enter the amount you want to deposit: ")
-		fmt.Scan(&depositAmount)
-		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
-		fmt.Println(" Balance updated!, Your account balance is: ", accountBalance)
+		if choice == 1 {
+			fmt.Println("Checking your balance")
+			fmt.Println("Your account balance is: ", accountBalance)
 
-	} else if choice == 3 {
-		fmt.Println("Withdraw")
-		fmt.Print("Enter the amount you want to withdraw: ")
-		fmt.Scan(&WithdrawAmount)
-		if WithdrawAmount > accountBalance {
-			fmt.Println("Insufficient balance")
+		} else if choice == 2 {
+			fmt.Println("Deposit")
+			fmt.Print("Enter the amount you want to deposit: ")
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid deposit amount")
+				return
+			}
+			accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+			fmt.Println(" Balance updated!, Your account balance is: ", accountBalance)
+
+		} else if choice == 3 {
+			fmt.Println("Withdraw")
+			fmt.Print("Enter the amount you want to withdraw: ")
+			fmt.Scan(&WithdrawAmount)
+			if WithdrawAmount > accountBalance {
+				fmt.Println("Insufficient balance")
+			} else {
+				accountBalance -= WithdrawAmount // accountBalance = accountBalance - WithdrawAmount
+				fmt.Println("Balance updated!, Your account balance is: ", accountBalance)
+			}
+
+		} else if choice == 4 {
+			fmt.Println("Exiting the Bank of Golang")
 		} else {
-			accountBalance -= WithdrawAmount // accountBalance = accountBalance - WithdrawAmount
-			fmt.Println("Balance updated!, Your account balance is: ", accountBalance)
+			fmt.Println("Invalid choice")
+
 		}
-
-	} else if choice == 4 {
-		fmt.Println("Exiting the Bank of Golang")
-	} else {
-		fmt.Println("Invalid choice")
-
 	}
+
 }
