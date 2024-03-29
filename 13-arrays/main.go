cf5021e18ad9ac236ceea2e8b4ed776143e2842c
ebrardev <16155052@mersin.edu.tr>
2024-03-29 23:45:46 +0300

    create slice

--- 13-arrays/main.go
+++ 13-arrays/main.go
@@ -12,8 +12,10 @@ func main() {
 	var productNames [4]string = [4]string{"tantuni"}
 	productNames[2] = "lahmacun"
 
-	prices := [4]float64{10.99, 5.99, 3.99, 7.99}
+	prices := [4]float64{10.99, 5.99, 3.99, 7.99} //? 0 1 2 3
 	fmt.Println(prices)
 	fmt.Println(productNames)
 	fmt.Println(prices[0])
+	feauturedPrices := prices[1:3]
+	fmt.Println(feauturedPrices)
 }
