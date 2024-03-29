538678702085593d0af21aee99a2850425115bde
ebrardev <16155052@mersin.edu.tr>
2024-03-30 01:56:42 +0300

    dynamic list

--- 13-arrays/main.go
+++ 13-arrays/main.go
@@ -2,28 +2,33 @@ package main
 
 import "fmt"
 
-type Product struct {
-	title string
-	id    string
-	price float64
-}
-
 func main() {
-	var productNames [4]string = [4]string{"tantuni"}
-	productNames[2] = "lahmacun"
+	prices := []float64{4.24, 5.55, 6.66}
+	fmt.Println(prices[0:1])
+	prices[1] = 5.99
+	prices[2] = 39.99
 
-	prices := [4]float64{10.99, 5.99, 3.99, 7.99} //? 0 1 2 3
 	fmt.Println(prices)
-	fmt.Println(productNames)
-	fmt.Println(prices[0])
-	feauturedPrices := prices[1:] //? 	feauturedPrices := prices[1:3]
-	feauturedPrices[0] = 1.99
-	highlightedPrices := feauturedPrices[:2]
-	fmt.Println(feauturedPrices)
-	fmt.Println(highlightedPrices)
+	prices = append(prices, 9.99)
 	fmt.Println(prices)
-	fmt.Println(len(feauturedPrices), cap(feauturedPrices))
-	fmt.Println(len(prices), cap(prices))
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
 }
+
+// func main() {
+// 	var productNames [4]string = [4]string{"tantuni"}
+// 	productNames[2] = "lahmacun"
+
+// 	prices := [4]float64{10.99, 5.99, 3.99, 7.99} //? 0 1 2 3
+// 	fmt.Println(prices)
+// 	fmt.Println(productNames)
+// 	fmt.Println(prices[0])
+// 	feauturedPrices := prices[1:] //? 	feauturedPrices := prices[1:3]
+// 	feauturedPrices[0] = 1.99
+// 	highlightedPrices := feauturedPrices[:2]
+// 	fmt.Println(feauturedPrices)
+// 	fmt.Println(highlightedPrices)
+// 	fmt.Println(prices)
+// 	fmt.Println(len(feauturedPrices), cap(feauturedPrices))
+// 	fmt.Println(len(prices), cap(prices))
+// 	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+// }
