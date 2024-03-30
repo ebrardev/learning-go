6b91329ad0b24c3e06818be42707081109f565a4
ebrardev <16155052@mersin.edu.tr>
2024-03-30 04:09:51 +0300

    Add maps and slices examples

--- 14-exercise-array/maps.go
+++ 14-exercise-array/maps.go
@@ -3,15 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	websites := map[string]string{
-		"Facebook": "https://facebook.com",
-		"Google":   "https://google.com",
-		"Twitter":  "https://twitter.com",
-	}
-	fmt.Println(websites)
-	fmt.Println(websites["Facebook"])
-	websites["AkrepNalan"] = "https://akrepnalan.com"
-	fmt.Println(websites)
-	delete(websites, "Facebook")
-	fmt.Println(websites)
+	//? using make function to create a slice
+
+	userNames := make([]string, 2)
+
+	userNames = append(userNames, "ali")
+	userNames = append(userNames, "veli")
+	userNames = append(userNames, "ahmet")
+	userNames = append(userNames, "mehmet")
+	fmt.Println(userNames)
+
+	//? using make function to create a map
+	courseRatings := make(map[string]float64, 3)
+
+	courseRatings["go"] = 5
+	courseRatings["react"] = 4.5
+	courseRatings["flutter"] = 4.7
+	fmt.Println(courseRatings)
+
 }
---
+++ 14-exercise-array/maps/maps.go
@@ -0,0 +1,18 @@
+package maps
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Facebook": "https://facebook.com",
+		"Google":   "https://google.com",
+		"Twitter":  "https://twitter.com",
+	}
+	fmt.Println(websites)
+	fmt.Println(websites["Facebook"])
+	websites["AkrepNalan"] = "https://akrepnalan.com"
+	fmt.Println(websites)
+	delete(websites, "Facebook")
+	fmt.Println(websites)
+
+}
