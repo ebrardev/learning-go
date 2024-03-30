206bd8ebca239574cd32b84683c92635f1b7063d
ebrardev <16155052@mersin.edu.tr>
2024-03-30 03:25:36 +0300

    introducing maps

---
+++ 14-exercise-array/go.mod
@@ -0,0 +1,3 @@
+module ebrardev.com/arrays
+
+go 1.22.0
---
+++ 14-exercise-array/maps.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Facebook": "https://facebook.com",
+		"Google":   "https://google.com",
+		"Twitter":  "https://twitter.com",
+	}
+	fmt.Println(websites)
+}
