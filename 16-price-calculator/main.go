ccf11247a1e9405fd2b8faca07ec5fd402b0875c
ebrardev <16155052@mersin.edu.tr>
2024-04-04 15:08:43 +0300

    Refactor import statements and fix syntax error in prices.go

--- 16-price-calculator/main.go
+++ 16-price-calculator/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
@@ -8,17 +10,6 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	result := make(map[float64][]float64)
-
-	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-
-			taxIncludedPrices[priceIndex] = price + (1 + taxRate)
-
-		}
-		result[taxRate] = taxIncludedPrices
-
-	}
 
 	fmt.Println(result)
 
--- 16-price-calculator/prices/prices.go
+++ 16-price-calculator/prices/prices.go
@@ -1,11 +1,26 @@
 package prices
 
+import "fmt"
+
 type taxIncludedPricesJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
 
+func (job taxIncludedPricesJob) Process() {
+	result := make(map[string]float64)
+	
+	
+		for priceIndex, price := range job.InputPrices {
+
+			result[fmt.Sprintf("%.2f",price)] = price + (1 + job.TaxRate)
+
+		}
+		result[taxRate] = taxIncludedPrices
+
+	}
+}
 func NewTaxIncludedPriceJob(taxRate float64) *taxIncludedPricesJob {
 	return &taxIncludedPricesJob{
 		InputPrices: []float64{10, 20, 30},
