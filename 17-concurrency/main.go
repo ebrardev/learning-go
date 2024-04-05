31f6d63b47e2f386f4b57eb7f9eda5066591f9de
ebrardev <16155052@mersin.edu.tr>
2024-04-05 11:24:49 +0300

    goroutines

--- 17-concurrency/main.go
+++ 17-concurrency/main.go
@@ -5,8 +5,9 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
@@ -16,10 +17,18 @@ func slowGreet(phrase string, doneChan chan bool) {
 }
 
 func main() {
-	// go greet("Nice to meet you!")
-	// go greet("How are you?")
-	done := make(chan bool)
-	go slowGreet("How ... are ... you ...?", done)
-	<-done
-	// go greet("I hope you're liking the course!")
+	dones := make([]chan bool, 4)
+	// done := make(chan bool)
+	dones[0] = make(chan bool)
+	go greet("Nice to meet you!", dones[0])
+	dones[1] = make(chan bool)
+	go greet("How are you?", dones[1])
+	dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", dones[2])
+	dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", dones[3])
+
+	for _, done := range dones {
+		<-done
+	}
 }
